bec8f2c99abf8ef240f49f7dda2e135c316f73b5
DataCodeLiteracy <dataliteracy@icloud.com>
2024-03-19 02:50:20 +0900

    [ 🚚 | | chore ] 불필요한 파일 삭제 & reset.css 적용
    2024.03.19

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,19 +1,17 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
+import AuthButton from '@/components/AuthButton'
+import { createClient } from '@/utils/supabase/server'
+import FetchDataSteps from '@/components/tutorial/FetchDataSteps'
+import { redirect } from 'next/navigation'
 
 export default async function ProtectedPage() {
-  const supabase = createClient();
+  const supabase = createClient()
 
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: { user }
+  } = await supabase.auth.getUser()
 
   if (!user) {
-    return redirect("/login");
+    return redirect('/login')
   }
 
   return (
@@ -25,14 +23,12 @@ export default async function ProtectedPage() {
         </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
             <AuthButton />
           </div>
         </nav>
       </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           <FetchDataSteps />
@@ -41,7 +37,7 @@ export default async function ProtectedPage() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Powered by{' '}
           <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
@@ -53,5 +49,5 @@ export default async function ProtectedPage() {
         </p>
       </footer>
     </div>
-  );
+  )
 }
