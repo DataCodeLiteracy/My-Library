96402047487347b12d5113de394dc0eb4cf24518
DataCodeLiteracy <dataliteracy@icloud.com>
2024-03-20 04:02:10 +0900

    [ ✨ | #2 | feature ] 로그아웃 기능 구현하기
    2024.03.20

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -4,12 +4,48 @@ import { IoMdSearch } from 'react-icons/io'
 import * as s from './Header.css'
 
 import Image from 'next/image'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
+import { supabase } from '@/utils/supabase/client'
+import { useMutation } from '@tanstack/react-query'
+import useAlertContext from '@/hooks/useAlertContext'
+import { removeLocalToken } from '@/utils/localToken'
+import authState from '@/recoil/authAtom'
+import { useSetRecoilState } from 'recoil'
 
 const EXCLUSION_PATHS = ['/login', '/register', '/reset-password']
 
 const Header = () => {
+  const setAuthStateValue = useSetRecoilState(authState)
+
+  const router = useRouter()
   const path = usePathname()
+  const { open, close } = useAlertContext()
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+  }
+
+  const { mutate: logout } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      removeLocalToken()
+      open({
+        title: '로그아웃 되었습니다.',
+        onRightButtonClick: () => {
+          close()
+          setAuthStateValue((prev) => ({ ...prev, isLoggedIn: false }))
+          router.push('/login')
+        }
+      })
+    },
+    onError: (error) => {
+      console.error(error)
+    }
+  })
+
+  const handleLogOut = () => {
+    logout()
+  }
 
   if (EXCLUSION_PATHS.includes(path)) return null
 
@@ -41,7 +77,7 @@ const Header = () => {
             />
             <IoMdSearch className={s.icon} />
           </div>
-          <button>로그아웃</button>
+          <button onClick={handleLogOut}>로그아웃</button>
         </div>
       </nav>
     </section>
