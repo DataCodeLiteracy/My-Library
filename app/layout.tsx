83d8665ee1db4457f2b12e4b262bc59e746e4d3d
DataCodeLiteracy <dataliteracy@icloud.com>
2024-03-28 09:10:46 +0900

    [ ✨ | | feature ] 모바일 자동 확대 안되도록 하기
    2024.03.28

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,13 @@ import { AlertContextProvider } from "@/contexts/AlertContext"
 import Providers from "@/react-query/Providers"
 
 import RecoilRootProvider from "@/recoil/RecoilRootProvider"
+import Head from "next/head"
 
-export const metadata = {
-  title: "My Library",
-  description: "My Library App"
-}
+// export const metadata = {
+//   title: "My Library",
+//   description: "My Library App",
+//   viewport: "width=device-width, initial-scale=1, maximum-scale=1"
+// }
 
 export default function RootLayout({
   children
@@ -20,6 +22,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ko'>
+      <Head>
+        <title>My Library</title>
+        <meta name='description' content='My Library App' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        />
+      </Head>
       <body>
         <RecoilRootProvider>
           <AlertContextProvider>
